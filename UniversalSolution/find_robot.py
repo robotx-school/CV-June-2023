ebe84bfebd1511948ca13e0eab0c6557f6b4e95d
Stephan Zhdanov <55328925+ret7020@users.noreply.github.com>
2023-06-15 12:32:01 +0300

    Update find_robot.py

--- UniversalSolution/find_robot.py
+++ UniversalSolution/find_robot.py
@@ -5,8 +5,12 @@ def find_robot(robot_zone_img):
     h_min = (0, 140, 173)
     h_max = (255, 255, 255)
     img_bin_r = cv2.inRange(robot_zone_img, h_min, h_max) 
-    bitwiseNot_r = cv2.bitwise_not(img_bin_r) 
-    summa = np.sum(bitwiseNot_r, axis=1) 
+    kernel = np.ones((5, 5), 'uint8')
+    thresh = cv2.erode(img_bin_r, kernel, iterations=4)
+    img_bin_r = cv2.dilate(thresh, kernel, iterations=3)
+    
+    bitwiseNot_r = cv2.bitwise_not(img_bin_r)
+    summa = np.sum(bitwiseNot_r, axis=1)
     dafk = np.where(summa > 10000) 
     
     if len(dafk[0]) > 1:
