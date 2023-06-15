2de2a58ed258623037a030267028acac88787a9a
Stepan Burmistrov <bursteand@gmail.com>
2023-06-15 12:52:26 +0300

    Update main.py

--- UniversalSolution/main.py
+++ UniversalSolution/main.py
@@ -27,22 +27,31 @@ from e_cam2map_cut import e_cam2map_cut
 visualizer = Visualize()
 
 while True:
+    visualizer.update()
     frame = get_image(HOST_IP, HOST_PORT)
     undistroted_image = undistort(frame, K, D)
     field_img = warp_field(undistroted_image, ARUCO_DICT)
     image_set = split_image(field_img,image_split_coords)
     robot_right_top, robot_right_bottom = find_robot(image_set['robot_right'])
     robot_left_top, robot_left_bottom = find_robot(image_set['robot_left'])
+    if robot_right_top is not None:
+        visualizer.draw_robot([(330, robot_right_top), (520, robot_right_bottom)])
+    if robot_left_top is not None:
+        visualizer.draw_robot([(110, robot_left_top), (310, robot_left_bottom)])
 
+    print(robot_right_top,robot_left_top)
+    
+    cv2.imshow("Visualization", visualizer.image)    
     right_robot_battery = find_robot_battery(image_set['robot_right'],17, ARUCO_DICT)
     left_robot_battery = find_robot_battery(image_set['robot_left'],17, ARUCO_DICT)
-
+    
 
     if right_robot_battery[0][0] is not None:
         field_coord_right_robot_battery = get_correct_coordinates(right_robot_battery,image_split_coords['robot_right_l_x'])
         mm_coord_right_robot_battery = list_average(e_cam2map_convert(e_cam2map_cut(field_img,image_split_coords)[0]),l_cam2map_convert(field_coord_right_robot_battery))
         left_manip_coord_right_robot_battery = get_l_manup_cords(mm_coord_right_robot_battery)
         right_manip_coord_right_robot_battery = get_r_manup_cords(mm_coord_right_robot_battery)
+        visualizer.draw_marker(mm_coord_right_robot_battery[0],mm_coord_right_robot_battery[1], 17, right_robot_battery[0][1])                                                         
     else:
         print('no right robot')
     
@@ -52,9 +61,11 @@ while True:
         mm_coord_left_robot_battery = list_average(e_cam2map_convert(e_cam2map_cut(field_img,image_split_coords)[1]),l_cam2map_convert(field_coord_left_robot_battery))
         left_manip_coord_left_robot_battery = get_l_manup_cords(mm_coord_left_robot_battery)
         right_manip_coord_left_robot_battery = get_r_manup_cords(mm_coord_left_robot_battery)
+        visualizer.draw_marker(mm_coord_left_robot_battery[0],mm_coord_left_robot_battery[1], 17, left_robot_battery[0][1])                                                         
     else:
         print('no left robot')
-
+        
+    
 
     
     is_bat_charged_up = check_marker(image_set['bat_charged_up'])
@@ -63,11 +74,9 @@ while True:
     is_bat_discharged_down = check_marker(image_set['bat_discharged_down'])
 
     #robot_control() Stas
-    visualizer.update()
-    visualizer.draw_robot([(100, 100), (150, 150)])  
-    visualizer.draw_marker(100, 100, 17, 45)                                                         
-    cv2.imshow("Visualization", visualizer.image)
-    cv2.imshow("Main", frame)
+    
+      
+    
     key = cv2.waitKey(10) & 0xFF
     if key == ord('q'):
         break
